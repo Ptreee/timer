9b8dfab06e01a8275830758c233f83ce3efcbb44
Piotr Proszowski <60212583+Ptreee@users.noreply.github.com>
2021-12-01 09:23:10 +0100

    refatoring, add timer core logic to timer.js

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause);
-  }
-
-  start = () => {
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  };
-  tick = () => {
-    console.log(`tick`);
-  };
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log(`timer started 😎`);
+  },
+  onTick() {
+    console.log(`tick tock`);
+  },
+  onComplete() {
+    console.log(`timer completed ✅`);
+  },
+});
